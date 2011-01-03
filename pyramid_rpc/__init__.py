25f748f0d2b40b99a58ea797b32140d7436794b3
Michael Merickel <michael@merickel.org>
2011-01-03 00:51:14 -0600

    Exposed JSONRPCError exceptions into pyramid_rpc namespace.

--- pyramid_rpc/__init__.py
+++ pyramid_rpc/__init__.py
@@ -1,4 +1,3 @@
 from pyramid_rpc.xmlrpc import xmlrpc_endpoint
 from pyramid_rpc.xmlrpc import xmlrpc_view
-from pyramid_rpc.jsonrpc import jsonrpc_endpoint
-from pyramid_rpc.jsonrpc import jsonrpc_view
+from pyramid_rpc.jsonrpc import *
