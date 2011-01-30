13985522f8557b032d10b38d25b9984b9024ef03
Ben Bangert <ben@groovie.org>
2011-01-29 23:25:29 -0800

    Refactor xmlrpc to use separate view_lookup function.

---
+++ pyramid_rpc/api.py
@@ -0,0 +1,28 @@
+"""RPC Utility methods
+
+These utility methods are intended for use by RPC functions to lookup
+qualifying view to call in response to an RPC method.
+
+"""
+from zope.interface import providedBy
+
+from pyramid.exceptions import NotFound
+from pyramid.interfaces import IRequest
+from pyramid.interfaces import IRouteRequest
+from pyramid.interfaces import IView
+from pyramid.interfaces import IViewClassifier
+
+
+def view_lookup(request, method):
+    registry = request.registry
+    adapters = registry.adapters
+    
+    # Hairy view lookup stuff below, woo!
+    request_iface = registry.queryUtility(
+        IRouteRequest, name=request.matched_route.name,
+        default=IRequest)
+    context_iface = providedBy(request.context)
+    view_callable = adapters.lookup(
+        (IViewClassifier, request_iface, context_iface),
+        IView, name=method, default=None)
+    return view_callable
