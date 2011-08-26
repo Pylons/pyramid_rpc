d4b40157a09f8914fb537c8da866698386aa0d2f
Michael Merickel <michael@merickel.org>
2011-08-25 23:53:11 -0500

    Added a new implementation of JSON-RPC.
    This exposes 2 methods: ``config.add_jsonrpc_endpoint`` and
    ``config.add_jsonrpc_method`` which are closely related to
    ``config.add_route`` and ``config.add_view``. It is now possible to use
    security and other route/view predicates with RPC calls.

--- pyramid_rpc/jsonrpc.py
+++ pyramid_rpc/jsonrpc.py
@@ -2,6 +2,9 @@ import inspect
 import logging
 
 from pyramid.compat import json
+from pyramid.exceptions import ConfigurationError
+from pyramid.httpexceptions import HTTPNoContent
+from pyramid.httpexceptions import HTTPNotFound
 from pyramid.interfaces import IViewMapperFactory, IViewMapper
 from pyramid.response import Response
 from zope.interface import implements, classProvides
@@ -222,3 +225,140 @@ def _call_rpc(request, body):
 
     request.rpc_args = rpc_args
     return view_callable(request.context, request)
+
+
+def exception_view(exc, request):
+    rpc_id = getattr(request, 'rpc_id', None)
+    return jsonrpc_error_response(exc, rpc_id)
+
+
+def notfound_view(exc, request):
+    rpc_id = getattr(request, 'rpc_id', None)
+    return jsonrpc_error_response(JsonRpcMethodNotFound(), rpc_id)
+
+
+def notification_view(exc, request):
+    return exc
+
+
+def jsonrpc_renderer(info):
+    def _render(value, system):
+        request = system.get('request')
+        if request is not None:
+            rpc_id = getattr(request, 'rpc_id', None)
+            if rpc_id is None:
+                raise HTTPNoContent()
+
+            response = request.response
+            ct = response.content_type
+            if ct == response.default_content_type:
+                response.content_type = 'application/json'
+
+            out = {
+                'jsonrpc' : JSONRPC_VERSION,
+                'id' : rpc_id,
+                'result' : value,
+            }
+            return json.dumps(out)
+    return _render
+
+
+def setup_jsonrpc(request):
+    try:
+        body = request.json_body
+    except ValueError:
+        raise JsonRpcParseError
+
+    request.rpc_id = body.get('id')
+    request.rpc_args = body.get('params', [])
+    request.rpc_method = body.get('method')
+    request.rpc_version = body.get('jsonrpc')
+
+    if request.rpc_version != JSONRPC_VERSION:
+        raise JsonRpcRequestInvalid
+
+    if request.rpc_method is None:
+        raise JsonRpcRequestInvalid
+
+
+def add_jsonrpc_endpoint(self, name, *args, **kw):
+    """ Add an endpoint for handling JSON-RPC.
+
+    name
+
+        The name of the endpoint.
+
+    A JSON-RPC method also accepts all of the arguments supplied to Pyramid's
+    ``add_route`` method.
+    """
+    def jsonrpc_endpoint_predicate(info, request):
+        # potentially setup either rpc v1 or v2 from the parsed body
+        setup_jsonrpc(request)
+
+        # Always return True so that even if it isn't a valid RPC it
+        # will fall through to the notfound_view which will still
+        # return a valid JSON-RPC response.
+        return True
+    predicates = kw.setdefault('custom_predicates', [])
+    predicates.append(jsonrpc_endpoint_predicate)
+    self.add_route(name, *args, **kw)
+    self.add_view(notfound_view, route_name=name, context=HTTPNotFound)
+    self.add_view(notification_view, route_name=name, context=HTTPNoContent)
+    self.add_view(exception_view, route_name=name, context=Exception)
+
+
+def add_jsonrpc_method(self, view, **kw):
+    """ Add a method to a JSON-RPC endpoint.
+
+    endpoint
+
+        The name of the endpoint.
+
+    method
+
+        The name of the method.
+
+    A JSON-RPC method also accepts all of the arguments supplied to Pyramid's
+    ``add_view`` method.
+    """
+    endpoint = kw.pop('endpoint', kw.pop('route_name', None))
+    if endpoint is None:
+        raise ConfigurationError(
+            'Cannot register a JSON-RPC endpoint without specifying the '
+            'name of the endpoint.')
+
+    method = kw.pop('method', None)
+    if method is None:
+        raise ConfigurationError(
+            'Cannot register a JSON-RPC method without specifying the '
+            '"method"')
+
+    def jsonrpc_method_predicate(context, request):
+        return getattr(request, 'rpc_method', None) == method
+    predicates = kw.setdefault('custom_predicates', [])
+    predicates.append(jsonrpc_method_predicate)
+    kw.setdefault('renderer', 'pyramid_rpc:jsonrpc')
+    self.add_view(view, route_name=endpoint, **kw)
+
+
+def includeme(config):
+    """ Set up standard configurator registrations.  Use via:
+
+    .. code-block:: python
+
+       config = Configurator()
+       config.include('pyramid_rpc.jsonrpc')
+
+    Once this function has been invoked, two new directives will be
+    available on the configurator:
+
+    - ``add_jsonrpc_endpoint``: Add an endpoint for handling JSON-RPC.
+
+    - ``add_jsonrpc_method``: Add a method to a JSON-RPC endpoint.
+
+    """
+    config.add_directive('add_jsonrpc_endpoint', add_jsonrpc_endpoint)
+    config.add_directive('add_jsonrpc_method', add_jsonrpc_method)
+    config.add_renderer('pyramid_rpc:jsonrpc', jsonrpc_renderer)
+    config.add_view(exception_view, context=JsonRpcError)
+
--- pyramid_rpc/tests/test_jsonrpc.py
+++ pyramid_rpc/tests/test_jsonrpc.py
@@ -3,6 +3,8 @@ import unittest
 from pyramid import testing
 from pyramid.compat import json
 
+from webtest import TestApp
+
 class TestJSONRPCMapper(unittest.TestCase):
     def _getTargetClass(self):
         from pyramid_rpc.jsonrpc import JsonRpcViewMapper
@@ -297,6 +299,187 @@ class TestJSONRPCEndPoint(unittest.TestCase):
 
         data = json.loads(response.body)
         self.assertEqual(data['error']['code'], JsonRpcRequestInvalid.code)
+
+class TestJSONRPCIntegration(unittest.TestCase):
+
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_add_jsonrpc_method_with_no_endpoint(self):
+        from pyramid.exceptions import ConfigurationError
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        with self.assertRaises(ConfigurationError):
+            config.add_jsonrpc_method(lambda r: None, method='dummy')
+
+    def test_add_jsonrpc_method_with_no_method(self):
+        from pyramid.exceptions import ConfigurationError
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        with self.assertRaises(ConfigurationError):
+            config.add_jsonrpc_method(lambda r: None, endpoint='rpc')
+
+    def test_it(self):
+        def view(request):
+            return request.rpc_args[0]
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        config.add_jsonrpc_method(view, endpoint='rpc', method='dummy')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'jsonrpc': '2.0', 'method': 'dummy', 'id': 5,
+                  'params': [2, 3]}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params=json.dumps(params))
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['id'], 5)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['result'], 2)
+
+    def test_it_with_multiple_methods(self):
+        def view(request):
+            return request.rpc_args[0]
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        config.add_jsonrpc_method(view, endpoint='rpc', method='dummy')
+        config.add_jsonrpc_method(lambda r: 'fail',
+                                  endpoint='rpc', method='dummy2')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'jsonrpc': '2.0', 'method': 'dummy', 'id': 5,
+                  'params': [2, 3]}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                        params=json.dumps(params))
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['id'], 5)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['result'], 2)
+
+    def test_it_with_no_version(self):
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'method': 'dummy', 'id': 5, 'params': [2, 3]}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params=json.dumps(params))
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['id'], 5)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['error']['code'], -32600)
+
+    def test_it_with_no_method(self):
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'jsonrpc': '2.0', 'id': 5, 'params': [2, 3]}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params=json.dumps(params))
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['id'], 5)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['error']['code'], -32600)
+
+    def test_it_with_no_id(self):
+        def view(request):
+            return request.rpc_args[0]
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        config.add_jsonrpc_method(view, endpoint='rpc', method='dummy')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'jsonrpc': '2.0', 'method': 'dummy', 'params': [2, 3]}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params=json.dumps(params))
+        self.assertEqual(resp.status_int, 204)
+        self.assertEqual(resp.body, '')
+
+    def test_it_with_no_params(self):
+        def view(request):
+            self.assertEqual(request.rpc_args, [])
+            return 'no params'
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        config.add_jsonrpc_method(view, endpoint='rpc', method='dummy')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'jsonrpc': '2.0', 'id': 5, 'method': 'dummy'}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params=json.dumps(params))
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['id'], 5)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['result'], 'no params')
+
+    def test_it_with_invalid_method(self):
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'jsonrpc': '2.0', 'id': 5, 'method': 'foo', 'params': [2, 3]}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params=json.dumps(params))
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['id'], 5)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['error']['code'], -32601)
+
+    def test_it_with_invalid_body(self):
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params='{')
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['error']['code'], -32700)
+
+    def test_it_with_general_exception(self):
+        def view(request):
+            raise Exception()
+        config = self.config
+        config.include('pyramid_rpc.jsonrpc')
+        config.add_jsonrpc_endpoint('rpc', '/api/jsonrpc')
+        config.add_jsonrpc_method(view, endpoint='rpc', method='dummy')
+        app = config.make_wsgi_app()
+        app = TestApp(app)
+        params = {'jsonrpc': '2.0', 'method': 'dummy', 'id': 5,
+                  'params': [2, 3]}
+        resp = app.post('/api/jsonrpc', content_type='application/json',
+                          params=json.dumps(params))
+        self.assertEqual(resp.status_int, 200)
+        self.assertEqual(resp.content_type, 'application/json')
+        result = json.loads(resp.body)
+        self.assertEqual(result['id'], 5)
+        self.assertEqual(result['jsonrpc'], '2.0')
+        self.assertEqual(result['error']['code'], -32603)
 
 class FunctionalTest(unittest.TestCase):
 
