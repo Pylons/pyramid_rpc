021cd270842dc52d340ba983194ab6c7a091a383
Michael Merickel <michael@merickel.org>
2011-08-28 16:41:40 -0500

    Removed the unreleased JSON-RPC implementation.

--- pyramid_rpc/__init__.py
+++ pyramid_rpc/__init__.py
@@ -1,5 +1,2 @@
 from pyramid_rpc.xmlrpc import xmlrpc_endpoint
 from pyramid_rpc.xmlrpc import xmlrpc_view
-from pyramid_rpc.jsonrpc import jsonrpc_endpoint
-#from pyramid_rpc.jsonrpc import jsonrpc_view
-rpc_view = xmlrpc_view
--- pyramid_rpc/jsonrpc.py
+++ pyramid_rpc/jsonrpc.py
@@ -8,17 +8,13 @@ from pyramid.httpexceptions import HTTPNotFound
 from pyramid.response import Response
 from pyramid.view import view_config
 
-from zope.deprecation import deprecated
-
 from pyramid_rpc.api import MapplyViewMapper
 from pyramid_rpc.api import view_lookup
 from pyramid_rpc.api import ViewMapperArgsInvalid
 
-__all__ = ['jsonrpc_endpoint']
 
 log = logging.getLogger(__name__)
 
-JSONRPC_VERSION = '2.0'
 
 class JsonRpcError(Exception):
     code = None
@@ -61,42 +57,14 @@ class JsonRpcInternalError(JsonRpcError):
     message = 'internal error'
 
 
-def jsonrpc_response(data, id=None):
-    """ Marshal a Python data structure into a webob ``Response``
-    object with a body that is a JSON string suitable for use as a
-    JSON-RPC response with a content-type of ``application/json``
-    and return the response."""
-
-    if id is None:
-        return Response(content_type="application/json")
-
-    out = {
-        'jsonrpc' : JSONRPC_VERSION,
-        'id' : id,
-        'result' : data,
-    }
-    try:
-        body = json.dumps(out)
-    except Exception:
-        return jsonrpc_error_response(JsonRpcInternalError(), id)
-
-    response = Response(body)
-    response.content_type = 'application/json'
-    response.content_length = len(body)
-    return response
-
-
 def jsonrpc_error_response(error, id=None):
     """ Marshal a Python Exception into a webob ``Response``
     object with a body that is a JSON string suitable for use as
     a JSON-RPC response with a content-type of ``application/json``
     and return the response."""
 
-    if not isinstance(error, JsonRpcError):
-        error = JsonRpcInternalError()
-
     body = json.dumps({
-        'jsonrpc' : JSONRPC_VERSION,
+        'jsonrpc' : '2.0',
         'id' : id,
         'error' : error.as_dict(),
     })
@@ -105,98 +73,6 @@ def jsonrpc_error_response(error, id=None):
     response.content_type = 'application/json'
     response.content_length = len(body)
     return response
-
-
-def jsonrpc_endpoint(request):
-    """A base view to be used with add_route to setup a JSON-RPC dispatch
-    endpoint
-    
-    Use this view with ``add_route`` to setup a JSON-RPC endpoint, for
-    example::
-        
-        config.add_route('RPC2', '/apis/jsonrpc', view=jsonrpc_endpoint)
-    
-    JSON-RPC methods should then be registered with ``add_view`` using the
-    route_name of the endpoint, the name as the jsonrpc method name. Or for
-    brevity, the :class:`~pyramid_rpc.rpc_view` decorator can be used.
-    
-    For example, to register an jsonrpc method 'list_users'::
-    
-        @rpc_view()
-        def list_users(request):
-            json_params = request.rpc_args
-            return {'users': [...]}
-    
-    Existing views that return a dict can be used with jsonrpc_view.
-    
-    """
-    length = request.content_length
-    if length == 0:
-        return jsonrpc_error_response(JsonRpcRequestInvalid())
-
-    try:
-        body = json.loads(request.body)
-    except ValueError:
-        return jsonrpc_error_response(JsonRpcParseError())
-
-    if isinstance(body, dict):
-        rpc_id = body.get('id')
-        if body.get('jsonrpc') != '2.0':
-            return jsonrpc_error_response(JsonRpcRequestInvalid(), rpc_id)
-        if 'method' not in body:
-            return jsonrpc_error_response(JsonRpcRequestInvalid(), rpc_id)
-        try:
-            data = _call_rpc(request, body)
-            return jsonrpc_response(data, rpc_id)
-        except Exception, e:
-            if rpc_id is None:
-                return Response(content_type="application/json")
-            return jsonrpc_error_response(e, rpc_id)
-    
-    if isinstance(body, list):
-        results = []
-        for b in body:
-            rpc_id = b.get('id')
-            try:
-                data = _call_rpc(request, b)
-                if rpc_id is not None:
-                    results.append(
-                    {'jsonrpc': '2.0', 'result': data, 'id': rpc_id})
-            except JsonRpcError, e:
-                results.append({'error': e.as_dict(), 'id': rpc_id})
-            except Exception, e:
-                if rpc_id is not None:
-                    e = JsonRpcInternalError(e)
-                    results.append({'error': e.as_dict(), 'id': rpc_id})
-
-        return Response(body=json.dumps(results),
-                       content_type="application/json") 
-
-    return jsonrpc_error_response(JsonRpcRequestInvalid())
-
-deprecated('jsonrpc_endpoint',
-           ('Deprecated as of pyramid_rpc 0.3, use the new API as described '
-            'in the documentation.'))
-
-
-def _call_rpc(request, body):
-    rpc_args = body.get('params', [])
-    rpc_method = body.get('method')
-    rpc_version = body.get('jsonrpc')
-
-    if rpc_version != JSONRPC_VERSION:
-        raise JsonRpcRequestInvalid
-
-    if not rpc_method:
-        raise JsonRpcRequestInvalid
-
-    view_callable = view_lookup(request, rpc_method)
-    log.debug('view callable %r found for method %r', view_callable, rpc_method)
-    if not view_callable:
-        raise JsonRpcMethodNotFound
-
-    request.rpc_args = rpc_args
-    return view_callable(request.context, request)
 
 
 def exception_view(exc, request):
@@ -233,7 +109,7 @@ def jsonrpc_renderer(info):
                 response.content_type = 'application/json'
 
             out = {
-                'jsonrpc' : JSONRPC_VERSION,
+                'jsonrpc' : '2.0',
                 'id' : rpc_id,
                 'result' : value,
             }
@@ -252,7 +128,7 @@ def setup_jsonrpc(request):
     request.rpc_method = body.get('method')
     request.rpc_version = body.get('jsonrpc')
 
-    if request.rpc_version != JSONRPC_VERSION:
+    if request.rpc_version != '2.0':
         raise JsonRpcRequestInvalid
 
     if request.rpc_method is None:
