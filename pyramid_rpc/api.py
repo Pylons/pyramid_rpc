5a45cd1fd68a4edcf57a6cae0b67f7fe8dc1e4e8
Michael Merickel <michael@merickel.org>
2011-08-28 02:21:49 -0500

    Carbon-copied the MapplyViewMapper from pyramid_xmlrpc.

--- pyramid_rpc/api.py
+++ pyramid_rpc/api.py
@@ -4,12 +4,16 @@ These utility methods are intended for use by RPC functions to lookup
 qualifying view to call in response to an RPC method.
 
 """
-from zope.interface import providedBy
+import inspect
 
 from pyramid.interfaces import IRequest
 from pyramid.interfaces import IRouteRequest
 from pyramid.interfaces import IView
 from pyramid.interfaces import IViewClassifier
+from pyramid.interfaces import IViewMapperFactory
+
+from zope.interface import implements
+from zope.interface import providedBy
 
 
 def view_lookup(request, method):
@@ -32,3 +36,90 @@ def view_lookup(request, method):
         (IViewClassifier, request_iface, context_iface),
         IView, name=method, default=None)
     return view_callable
+
+
+class MapplyViewMapper(object): 
+    implements(IViewMapperFactory)
+    def __init__(self, **kw):
+        self.attr = kw.get('attr')
+
+    def __call__(self, view):
+        attr = self.attr
+        if inspect.isclass(view):
+            def _class_view(context, request):
+                params = getattr(request, 'rpc_args', ())
+                keywords = dict(request.params.items())
+                if request.matchdict:
+                    keywords.update(request.matchdict)
+                if attr is None:
+                    inst = view(request)
+                    response = self.mapply(inst, params, keywords)
+                else:
+                    inst = view(request)
+                    response = self.mapply(getattr(inst, attr), params,
+                                           keywords)
+                request.__view__ = inst
+                return response
+            mapped_view = _class_view
+        else:
+            def _nonclass_view(context, request):
+                params = (request,) + getattr(request, 'rpc_args', ())
+                keywords = dict(request.params.items())
+                if request.matchdict:
+                    keywords.update(request.matchdict)
+                if attr is None:
+                    response = self.mapply(view, params, keywords)
+                else:
+                    response = self.mapply(getattr(view, attr), params,
+                                           keywords)
+                return response
+            mapped_view = _nonclass_view
+
+        return mapped_view
+
+    def mapply(self, ob, positional, keyword):
+
+        f = ob
+        im = False
+
+        if hasattr(f, 'im_func'):
+            im = True
+
+        elif not hasattr(f, 'func_defaults'):
+            if hasattr(f, '__call__'):
+                f = f.__call__
+                if hasattr(f, 'im_func'):
+                    im = True
+
+        if im:
+            f = f.im_func
+            c = f.func_code
+            defaults = f.func_defaults
+            names = c.co_varnames[1:c.co_argcount]
+        else:
+            defaults = f.func_defaults
+            c = f.func_code
+            names = c.co_varnames[:c.co_argcount]
+
+        nargs = len(names)
+        args = []
+        if positional:
+            positional = list(positional)
+            if len(positional) > nargs:
+                raise TypeError('too many arguments')
+            args = positional
+
+        get = keyword.get
+        nrequired = len(names) - (len(defaults or ()))
+        for index in range(len(args), len(names)):
+            name = names[index]
+            v = get(name, args)
+            if v is args:
+                if index < nrequired:
+                    raise TypeError('argument %s was omitted' % name)
+                else:
+                    v = defaults[index-nrequired]
+            args.append(v)
+
+        args = tuple(args)
+        return ob(*args)
---
+++ pyramid_rpc/tests/test_api.py
@@ -0,0 +1,147 @@
+import unittest
+from pyramid import testing
+
+class TestMapplyViewMapper(unittest.TestCase):
+    def _makeOne(self, **kw):
+        from pyramid_rpc.api import MapplyViewMapper
+        return MapplyViewMapper(**kw)
+
+    def test___call__isfunc_no_rpc_args_no_attr(self):
+        data = '123'
+        def view(request):
+            return data
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        mapper = self._makeOne()
+        result = mapper(view)(context, request)
+        self.assertEqual(result, data)
+
+    def test___call__isfunc_no_rpc_args_with_attr(self):
+        view = lambda *arg: 'wrong'
+        data = '123'
+        def foo(request):
+            return data
+        view.foo = foo
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        mapper = self._makeOne(attr='foo')
+        result = mapper(view)(context, request)
+        self.assertEqual(result, data)
+
+    def test___call__isfunc_with_rpc_args(self):
+        def view(request, a, b):
+            return a, b
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        request.rpc_args = ('a', 'b')
+        mapper = self._makeOne()
+        result = mapper(view)(context, request)
+        self.assertEqual(result, ('a', 'b'))
+
+    def test___call__isfunc_with_rpc_args_and_matchdict(self):
+        def view(request, a, b, c=1):
+            return a, b, c
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        request.rpc_args = ('a', 'b')
+        request.matchdict = dict(c='2')
+        mapper = self._makeOne()
+        result = mapper(view)(context, request)
+        self.assertEqual(result, ('a', 'b', '2'))
+
+    def test___call__isclass_no_rpc_args_no_attr(self):
+        data = '123'
+        class view(object):
+            def __init__(self, request):
+                pass
+            def __call__(self):
+                return data
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        mapper = self._makeOne()
+        result = mapper(view)(context, request)
+        self.assertEqual(result, data)
+        self.assertEqual(request.__view__.__class__, view)
+
+    def test___call__isclass_no_rpc_args_with_attr(self):
+        view = lambda *arg: 'wrong'
+        data = '123'
+        class view(object):
+            def __init__(self, request):
+                pass
+            def index(self):
+                return data
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        mapper = self._makeOne(attr='index')
+        result = mapper(view)(context, request)
+        self.assertEqual(result, data)
+        self.assertEqual(request.__view__.__class__, view)
+
+    def test___call__isclass_with_rpc_args(self):
+        class view(object):
+            def __init__(self, request):
+                pass
+            def __call__(self, a, b):
+                return a, b
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        request.rpc_args = ('a', 'b')
+        mapper = self._makeOne()
+        result = mapper(view)(context, request)
+        self.assertEqual(result, ('a', 'b'))
+
+    def test___call__isclass_with_rpc_args_and_matchdict(self):
+        class view(object):
+            def __init__(self, request):
+                pass
+            def __call__(self, a, b, c=1):
+                return a, b, c
+        context = testing.DummyResource()
+        request = testing.DummyRequest()
+        request.rpc_args = ('a', 'b')
+        request.matchdict = dict(c='2')
+        mapper = self._makeOne()
+        result = mapper(view)(context, request)
+        self.assertEqual(result, ('a', 'b', '2'))
+
+    def test_mapply_toomanyargs(self):
+        def aview(one, two): pass
+        mapper = self._makeOne()
+        self.assertRaises(TypeError, mapper.mapply, aview, (1, 2, 3), {})
+
+    def test_mapply_all_kwarg_arg_omitted(self):
+        def aview(one, two): pass
+        mapper = self._makeOne()
+        self.assertRaises(TypeError, mapper.mapply, aview, (), dict(one=1))
+
+    def test_mapply_all_kwarg_arg_omitted_with_default(self):
+        def aview(one, two=2):
+            return one, two
+        mapper = self._makeOne()
+        result = mapper.mapply(aview, (), dict(one=1))
+        self.assertEqual(result, (1, 2))
+
+    def test_mapply_all_kwarg(self):
+        def aview(one, two):
+            return one, two
+        mapper = self._makeOne()
+        result = mapper.mapply(aview, (), dict(one=1, two=2))
+        self.assertEqual(result, (1, 2))
+
+    def test_mapply_instmethod(self):
+        mapper = self._makeOne()
+        result = mapper.mapply(self._aview, ('a',), {})
+        self.assertEqual(result, 'a')
+
+    def test_mapply_inst(self):
+        class Foo(object):
+            def __call__(self, a):
+                return a
+        foo = Foo()
+        mapper = self._makeOne()
+        result = mapper.mapply(foo, ('a',), {})
+        self.assertEqual(result, 'a')
+
+    def _aview(self, a):
+        return a
