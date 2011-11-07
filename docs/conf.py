b2030edaaf46ea413af5db16c8b576948d795184
Reed O'Brien <reed@koansys.com>
2011-11-07 15:42:41 -0800

    doc update for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -105,6 +105,12 @@ sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
 html_theme = 'pyramid'
 
+
+html_theme_options = {
+    'github_url': 'https://github.com/Pylons/pyramid_rpc'
+}
+
+
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
@@ -131,7 +137,7 @@ html_theme = 'pyramid'
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['.static']
+# html_static_path = ['.static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
