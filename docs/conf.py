0f7ec9ca1e5f335df0f7ddf596cfbcd023179731
Michael Merickel <michael@merickel.org>
2012-10-13 20:54:48 -0500

    updated sphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,11 @@ extensions = [
     ]
 
 # Looks for bfg's objects
-intersphinx_mapping = {'http://docs.pylonsproject.org/projects/pyramid/dev': None}
+intersphinx_mapping = {
+    'pyramid': (
+        'http://docs.pylonsproject.org/projects/pyramid/en/latest/',
+        None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
