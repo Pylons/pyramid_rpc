db66d4f796579037ad718cf016c1e927eac0c7d8
Michael Merickel <michael@merickel.org>
2012-11-06 02:02:53 -0600

    added is_nonstr_iter compatability function

--- pyramid_rpc/compat.py
+++ pyramid_rpc/compat.py
@@ -7,3 +7,12 @@ if PY3: # pragma: no cover
     import xmlrpc.client as xmlrpclib
 else:
     import xmlrpclib
+
+if PY3: # pragma: no cover
+    def is_nonstr_iter(v):
+        if isinstance(v, str):
+            return False
+        return hasattr(v, '__iter__')
+else:
+    def is_nonstr_iter(v):
+        return hasattr(v, '__iter__')
