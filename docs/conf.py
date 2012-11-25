7546671e37239d1f53948412a030e658500601c3
Michael Merickel <michael@merickel.org>
2012-11-25 17:35:28 -0600

    prep 0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -55,9 +55,9 @@ copyright = '%s, Ben Bangert <ben@groovie.org>' % datetime.datetime.now().year
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.6'
+version = '0.5.1'
 # The full version, including alpha/beta/rc tags.
-release = '0.6dev'
+release = version
 
 # There are two options for replacing |today|: either, you set today to
 # some non-false value, then it is used:
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ docs_require = [
 ]
 
 setup(name='pyramid_rpc',
-      version='0.6dev',
+      version='0.5.1',
       description='RPC support for the Pyramid web framework',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
