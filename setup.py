61195ae42ced11d6d2c5f5da59c8b841313be34d
Michael Merickel <michael@merickel.org>
2015-05-15 10:34:47 -0500

    prep 0.6.1

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ docs_require = [
 ]
 
 setup(name='pyramid_rpc',
-      version='0.6',
+      version='0.6.1',
       description='RPC support for the Pyramid web framework',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
